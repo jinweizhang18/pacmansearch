16908c70ca58489610b32a5035922c804b1406a5
Jinwei Zhang <jinweizhang18@gmail.com>
2020-09-05 18:25:00 -0700

    part 5, in progress

--- searchAgents.py
+++ searchAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -195,7 +195,7 @@ class PositionSearchProblem(search.SearchProblem):
         successors = []
         for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
             x,y = state
-            dx, dy = Actions.directionToVector(action)
+            dx, dy = Actions.directionToVector(action) #IF NORTH dx = 0, dy = 1
             nextx, nexty = int(x + dx), int(y + dy)
             if not self.walls[nextx][nexty]:
                 nextState = (nextx, nexty)
@@ -280,6 +280,8 @@ class CornersProblem(search.SearchProblem):
         self.walls = startingGameState.getWalls()
         self.startingPosition = startingGameState.getPacmanPosition()
         top, right = self.walls.height-2, self.walls.width-2
+        self.top = top
+        self.right = right
         self.corners = ((1,1), (1,top), (right, 1), (right, top))
         for corner in self.corners:
             if not startingGameState.hasFood(*corner):
@@ -287,22 +289,36 @@ class CornersProblem(search.SearchProblem):
         self._expanded = 0 # DO NOT CHANGE; Number of search nodes expanded
         # Please add any code here which you would like to use
         # in initializing the problem
-        "*** YOUR CODE HERE ***"
+        self.cornersboolean = 0x0000 #((1,1), (1,top), (right, 1), (right, top))
+
 
     def getStartState(self):
         """
         Returns the start state (in your state space, not the full Pacman state
         space)
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.startingPosition
+
 
     def isGoalState(self, state):
         """
         Returns whether this search state is a goal state of the problem.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if state in self.corners:
+            print(state)
+            if list(state) == [1,1]:
+                print(list(state))
+                self.cornersboolean = self.cornersboolean | 0x1000
+            elif list(state) == [1, self.top]:
+                self.cornersboolean = self.cornersboolean | 0x0100
+            elif list(state) == [self.right, 1]:
+                self.cornersboolean = self.cornersboolean | 0x0010
+            elif list(state) == [self.right,self.top]:
+                self.cornersboolean = self.cornersboolean | 0x0001
+            if self.cornersboolean == 0x1111:
+                return True
+        else:
+            return False
 
     def getSuccessors(self, state):
         """
@@ -317,14 +333,13 @@ class CornersProblem(search.SearchProblem):
 
         successors = []
         for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
-            # Add a successor state to the successor list if the action is legal
-            # Here's a code snippet for figuring out whether a new position hits a wall:
-            #   x,y = currentPosition
-            #   dx, dy = Actions.directionToVector(action)
-            #   nextx, nexty = int(x + dx), int(y + dy)
-            #   hitsWall = self.walls[nextx][nexty]
-
-            "*** YOUR CODE HERE ***"
+            x,y = state
+            dx, dy = Actions.directionToVector(action) #IF NORTH dx = 0, dy = 1
+            nextx, nexty = int(x + dx), int(y + dy)
+            if not self.walls[nextx][nexty]:
+                nextState = (nextx, nexty)
+                cost = 1 #self.costFn(nextState)
+                successors.append( ( nextState, action, cost) )
 
         self._expanded += 1 # DO NOT CHANGE
         return successors
